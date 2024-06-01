396582e0aa9e696670e49dc7505b11f6a9dd5308
ashiqrahman10 <v.p.ashiq9790@gmail.com>
2024-06-02 00:20:21 +0530

    removed unnecessary folders

--- firebase/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import firebase_admin
-from dotenv import load_dotenv
-from firebase_admin import credentials
-
-load_dotenv()
-cred = credentials.Certificate(eval(os.getenv("FIREBASE_KEY")))
-
-admin = firebase_admin.initialize_app(cred)
